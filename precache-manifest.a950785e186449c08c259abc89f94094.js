f7b70d39f0d10a711842f13a4dd89f7bc27e8102
smeee <smeee23@gmail.com>
2021-11-05 20:12:49 +0000

    Updates

---
+++ precache-manifest.a950785e186449c08c259abc89f94094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515c65bf315c4e441b8f02b2e4a7865d",
+    "url": "/just_cause/index.html"
+  },
+  {
+    "revision": "5596f87e0e246b799d30",
+    "url": "/just_cause/static/css/main.3139e805.chunk.css"
+  },
+  {
+    "revision": "06d8b188540dd0d7f01c",
+    "url": "/just_cause/static/js/2.d67261dd.chunk.js"
+  },
+  {
+    "revision": "5596f87e0e246b799d30",
+    "url": "/just_cause/static/js/main.3ee4eb78.chunk.js"
+  },
+  {
+    "revision": "bc21b69397e7e23d8860",
+    "url": "/just_cause/static/js/runtime-main.12d4dc5f.js"
+  }
+]);

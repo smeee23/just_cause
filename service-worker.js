40c2721367781ecf780dfc7593e28f5241337e99
smeee <smeee23@gmail.com>
2022-03-16 20:22:40 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/just_cause/precache-manifest.0aa3365cf35ce383b29b0079b7fd1329.js"
+  "/just_cause/precache-manifest.6e2096e464ad1b8e6cf8aee2d425757b.js"
 );
 
 self.addEventListener('message', (event) => {

986e8d3858d693e597a7436efb4daf07a4ecf5e2
smeee <smeee23@gmail.com>
2022-04-29 01:31:30 +0000

    undo remove takeover from header

--- client/src/js/components/Header.js
+++ client/src/js/components/Header.js
@@ -81,6 +81,9 @@ class Header extends Component {
     )
 		return (
       <header className="app-bar horizontal-padding">
+        <Takeover>
+          { nav }
+        </Takeover>
         <NavLink exact to={"/"} className="app-bar__left tdn">
           <Logo/>
           <h2 className="mb0">JustCause</h2>

905906e66ff620259eb475741da82fe135dd3aca
smeee <smeee23@gmail.com>
2022-08-09 03:30:52 +0000

    change how price feed handles errors

--- client/src/js/func/priceFeeds.js
+++ client/src/js/func/priceFeeds.js
@@ -1,28 +1,31 @@
 const axios = require('axios');
 
 export const getPriceFromMessari = async(apiKey) => {
+    let priceUSD;
     try{
         const url = "https://data.messari.io/api/v1/assets/"+apiKey+"/metrics/market-data";
         const response = await axios.get(url);
-        const priceUSD = response.data.data.market_data.price_usd;
-        return priceUSD;
+        priceUSD = response.data.data.market_data.price_usd;
     }
     catch (error) {
         console.error(error);
     }
+    return priceUSD;
 }
 
 export const getPriceFromCoinGecko = async(networkId) => {
+    let response;
     try{
         if(networkId === 80001 || networkId === 137){
             const url = "https://api.coingecko.com/api/v3/simple/price?ids=aave,dai,tether,usd-coin,ethereum,bitcoin,chainlink,matic-network,defipulse-index&vs_currencies=usd";
             const response = await axios.get(url);
             console.log('coingecko prices:', response.data);
-            return response.data;
+            response = response.data;
         }
     }
     catch (error) {
         console.error(error);
     }
+    return response;
 }
 

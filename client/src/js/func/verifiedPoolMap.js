690ea6a3f4b7ad41c2ec3d337910641fe77d05a4
smeee <smeee23@gmail.com>
2022-08-13 14:45:54 +0000

    add verified pool for coin center

--- client/src/js/func/verifiedPoolMap.js
+++ client/src/js/func/verifiedPoolMap.js
@@ -1,4 +1,5 @@
 export const verifiedPoolMap = {
     GitcoinGrantsMatchingPool: {siteUrl: 'https://gitcoin.co/about'},
     UnchainFund: {siteUrl:'https://unchain.fund/'},
+    CoinCenter: {siteUrl: 'https://www.coincenter.org/'},
 }
